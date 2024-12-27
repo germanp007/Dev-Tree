440cfc955adb3ce3123fcfc337bc1dba6ce8f601
germanp007 <pintogerman281@gmail.com>
2024-12-27 09:55:15 -0300

    Configurando Cloudinary y .env

--- frontend/src/views/ProfileView.tsx
+++ frontend/src/views/ProfileView.tsx
@@ -1,6 +1,49 @@
+import { useForm } from "react-hook-form";
+import { ErrorMessage } from "../components/ErrorMessage";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { ProfileForm, User } from "../types";
+import { updateProfile } from "../api/DevTreeApi";
+import { toast } from "sonner";
+
 export default function ProfileView() {
+  const queryClient = useQueryClient();
+  const data: User = queryClient.getQueryData(["user"])!;
+
+  console.log(data, "DATA");
+
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ProfileForm>({
+    defaultValues: {
+      handle: data.handle,
+      description: data.description,
+    },
+  });
+
+  const updateProfileMutation = useMutation({
+    mutationFn: updateProfile,
+    onError: (error) => {
+      toast.error(error.message);
+    },
+    onSuccess: (data) => {
+      toast.success(data);
+      queryClient.invalidateQueries({ queryKey: ["user"] });
+    },
+  });
+
+  const handleUserProfileForm = (formData: ProfileForm) => {
+    console.log(formData, "Formdata");
+    updateProfileMutation.mutate(formData);
+    reset();
+  };
   return (
-    <form className="bg-white p-10 rounded-lg space-y-5" onSubmit={() => {}}>
+    <form
+      className="bg-white p-10 rounded-lg space-y-5"
+      onSubmit={handleSubmit(handleUserProfileForm)}
+    >
       <legend className="text-2xl text-slate-800 text-center">
         Editar Información
       </legend>
@@ -10,7 +53,11 @@ export default function ProfileView() {
           type="text"
           className="border-none bg-slate-100 rounded-lg p-2"
           placeholder="handle o Nombre de Usuario"
+          {...register("handle", {
+            required: "El Nombre de usuario es obligatorio",
+          })}
         />
+        {errors.handle && <ErrorMessage>{errors.handle.message}</ErrorMessage>}
       </div>
 
       <div className="grid grid-cols-1 gap-2">
@@ -18,7 +65,14 @@ export default function ProfileView() {
         <textarea
           className="border-none bg-slate-100 rounded-lg p-2"
           placeholder="Tu Descripción"
+          {...register("description", {
+            required: "La descripcion es obligatoria",
+          })}
         />
+
+        {errors.description && (
+          <ErrorMessage>{errors.description.message}</ErrorMessage>
+        )}
       </div>
 
       <div className="grid grid-cols-1 gap-2">
